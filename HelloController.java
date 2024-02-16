69664564b626f27c7c0f3cc16c7ddf3c04f6603a
Aaron Joshua Emmanuel <148550100+AaronJE45@users.noreply.github.com>
2024-02-16 23:18:49 +0530

    HelloController.java

--- HelloController.java
+++ HelloController.java
@@ -3,7 +3,7 @@ package org.example.help_learning;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class Controller {
+public class HelloController {
     @FXML
     private Label welcomeText;
 
